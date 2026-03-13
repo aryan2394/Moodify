0b8b9d14e56be2d1ae4cc76603af7059117fed91
aryankumar <70devirkt@gmail.com>
2026-03-13 16:47:26 +0530

    redis by shriji

---
+++ Day115/backend/src/models/user.model.js
@@ -0,0 +1,20 @@
+const mongoose=require("mongoose");
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        unique:[true,"username should be unique by shri ji"],
+        required:[true,"username is required by shri ji"]
+    },
+    email:{
+        type:String,
+        unique:[true,"email should be unique by shri ji"],
+        required:[true,"email should be required"],
+    },
+    password:{
+        type:String,
+        required:[true,"password is required"],
+        select:false
+    }
+})
+const userModel=mongoose.model("users",userSchema);
+module.exports=userModel;
